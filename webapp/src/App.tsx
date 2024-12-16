949d9f503fbfbf08c3635c4d74afb64f80499761
Andrejs Jakunins <duckssupplier@gmail.com>
2024-12-17 01:34:26 +0200

    add Prettier configuration and update ESLint rules; format code with single quotes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,31 +1,31 @@
 export const App = () => {
   const ideas = [
     {
-      nick: "cool-idea-nick-1",
-      name: "Idea 1",
-      description: "Idea 1 description",
+      nick: 'cool-idea-nick-1',
+      name: 'Idea 1',
+      description: 'Idea 1 description',
     },
     {
-      nick: "cool-idea-nick-2",
-      name: "Idea 2",
-      description: "Idea 2 description",
+      nick: 'cool-idea-nick-2',
+      name: 'Idea 2',
+      description: 'Idea 2 description',
     },
     {
-      nick: "cool-idea-nick-3",
-      name: "Idea 3",
-      description: "Idea 3 description",
+      nick: 'cool-idea-nick-3',
+      name: 'Idea 3',
+      description: 'Idea 3 description',
     },
     {
-      nick: "cool-idea-nick-4",
-      name: "Idea 4",
-      description: "Idea 4 description",
+      nick: 'cool-idea-nick-4',
+      name: 'Idea 4',
+      description: 'Idea 4 description',
     },
     {
-      nick: "cool-idea-nick-5",
-      name: "Idea 5",
-      description: "Idea 5 description",
+      nick: 'cool-idea-nick-5',
+      name: 'Idea 5',
+      description: 'Idea 5 description',
     },
-  ];
+  ]
   return (
     <div>
       <h1>Ideanick</h1>
@@ -36,5 +36,5 @@ export const App = () => {
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
